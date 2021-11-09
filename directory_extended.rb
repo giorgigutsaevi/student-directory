fb4a639840391a3b6161d20eeaf173429ba86d48
Giorgi Gutsaev <giorgi.gutsaevi@gmail.com>
2021-11-09 20:35:40 +0000

    Minor refactoring

--- directory_extended.rb
+++ directory_extended.rb
@@ -9,7 +9,7 @@
 # 7. In the input_students method the cohort value is hard-coded. How can you ask for both the name and the cohort? What if one of the values is empty? Can you supply a default value? The input will be given to you as a string? How will you convert it to a symbol? What if the user makes a typo? ✅
 # 8. Once you complete the previous exercise, change the way the users are displayed: print them grouped by cohorts. To do this, you'll need to get a list of all existing cohorts (the map() method may be useful but it's not the only option), iterate over it and only print the students from that cohort. ✅
 # 9. Right now if we have only one student, the user will see a message "Now we have 1 students", whereas it should be "Now we have 1 student". How can you fix it so that it uses the singular form when appropriate and plural form otherwise? ✅
-# 10. We've been using the rstrip() method to get rid of the last return character. Find another method among those provided by the String class that could be used for the same purpose (although it will require passing some arguments).
+# 10. We've been using the rstrip() method to get rid of the last return character. Find another method among those provided by the String class that could be used for the same purpose (although it will require passing some arguments). ✅
 
 
 def input_students
